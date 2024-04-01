bfe15d6c552b8f6b6d1c02a6f1a01543f0d38702
Shuja <shuja.ashraf11@gmail.com>
2024-04-01 19:02:14 +0000

    implemented slices

--- src/Store.js
+++ src/Store.js
@@ -1,53 +1,6 @@
 import { combineReducers, createStore } from "redux";
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-
-const initialStateCustomer = {
-  fullName: "",
-  nationalId: "",
-  createdAt: "",
-};
-
-function AccountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return { ...state, loan: 0, balance: state.balance - state.loan };
-    default:
-      return state;
-  }
-}
-
-function CustomerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullname,
-        nationalId: action.payload.nationalId,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
+import CustomerReducer from "./features/customers/customerSlice";
+import AccountReducer from "./features/accounts/accountSlice";
 
 const rootReducer = combineReducers({
   account: AccountReducer,
@@ -55,57 +8,4 @@ const rootReducer = combineReducers({
 });
 const store = createStore(rootReducer);
 
-// store.dispatch({ type: "account/deposit", payload: 500 });
-// store.dispatch({ type: "account/withdraw", payload: 100 });
-// store.dispatch({
-//   type: "account/requestLoan",
-//   payload: { amount: 2000, purpose: "Buy iPhone" },
-// });
-// store.dispatch({ type: "account/payLoan" });
-// console.log(store.getState());
-
-//using action creator function
-
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(deposit(500));
-store.dispatch(withdraw(100));
-store.dispatch(requestLoan(2000, "Buy iPhone"));
-store.dispatch(payLoan());
-// console.log(store.getState());
-
-function createCustomer(fullName, nationalId) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName,
-      nationalId,
-      createdAt: new Date().toISOString(),
-    },
-  };
-}
-function updateName(fullName) {
-  return {
-    type: "customer/updateName",
-    payload: fullName,
-  };
-}
-
-store.dispatch(createCustomer("Shuja Shah", "123433232"));
-store.dispatch(updateName("Shuja Shah"));
-
-console.log(store.getState());
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-import "./Store";
+import store from "./Store";
+store.dispatch({ type: "account/deposit", payload: 1000 });
+store.dispatch({
+  type: "customer/createCustomer",
+  payload: {
+    fullname: "John Doe",
+    nationalId: "123456789",
+    createdAt: new Date().toISOString(),
+  },
+});
+console.log(store.getState());
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
