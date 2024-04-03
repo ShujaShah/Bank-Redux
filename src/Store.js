0ee6cb00dd543cc50f5df547fe30db8c9fca8e69
Shuja <shuja.ashraf11@gmail.com>
2024-04-03 18:11:26 +0000

    bug fix of usd

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import CustomerReducer from "./features/customers/customerSlice";
 import AccountReducer from "./features/accounts/accountSlice";
@@ -8,6 +9,9 @@ const rootReducer = combineReducers({
   account: AccountReducer,
   customer: CustomerReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
